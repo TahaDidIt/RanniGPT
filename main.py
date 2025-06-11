88a2e8e7f3fae23f25dd8831c85629cc1e2a97a7
TahaDidIt <taha.majid4487@gmail.com>
2025-06-11 10:47:37 +0100

    put script into main and text splitting methods

--- main.py
+++ main.py
@@ -31,153 +31,164 @@ CODE STRUCTURE:
 
 
 
-### CONFIG
+def textSplitter(temp_dir):
+    # Load sentences.txt - temp folder by default
+    with open(os.path.join(temp_dir, "sentences.txt"), "r", encoding = "utf-8") as file:
+        text = file.read()
 
-# Directory setup
-base_dir = os.path.dirname(os.path.abspath(__file__))
-#By default output audio goes to temp folder
-temp_dir = os.path.join(base_dir, "temp")
+    # Split sentences apart for sequential batched inference
+    """
+    HOW THIS WORKS:
+    1. We use the `re` module for splitting text based on punctuation.
+    2. The regex `(\.{3}|[.!?])` ensures we handle:
+        - Ellipses (`...`) as a single punctuation unit.
+        - or Full stops (`.`), exclamation marks (`!`), and question marks (`?`).
+    3. Capturing groups `()` in the regex allow punctuation to be retained in the results rather than lost upon splitting.
+    4. Whitespace and empty strings are removed by strip() to ensure clean results.
+    5. How the if condition for .strip() works:
+        - Python accepts an empty sentence ('') as a result of .strip() as False, anything else true
+        - So it gets a true for non empty sentences and knows it can strip
+    6. After splitting, we combine each sentence with its respective punctuation by iterating
+    through the list in pairs (sentence + punctuation) for the sake of the TTS engine.
+    """
+    input_text = [sentence.strip() for sentence in re.split(r"(\.{3}|[.!?])", text) if sentence.strip()]
 
+    # combine sentences back with their punctuation mark
+    sentences = [
+        input_text[i] + input_text[i + 1]
+        for i in range(0, len(input_text) - 1, 2)  # range(inclusive, exclusive, step)
+        #We stop it 1 element before the end to prevent error if last element has no punc.
+    ]
+    print("SENTENCES:\n", sentences)
 
-# Model file locations
-
-#XTTS base model vocab file
-xtts_vocab_path = os.path.join(base_dir, "voices\\xtts\\vocab.json")
-#XTTS trained model checkpoint file
-xtts_checkpoint_path = os.path.join(base_dir, "voices\\xtts\\Ranni1\\best_model.pth")
-#XTTS trained model config
-xtts_config_path = os.path.join(base_dir, "voices\\xtts\\Ranni1\\config.json")
-#RVC trained model file location
-rvc_models_dir = os.path.join(base_dir, "voices\\rvc")
-
-
-
-
-
-### PARAMETERS 
-
-# REFERENCE AUDIO - by default looks in temp folder
-speaker_ref_path = os.path.join(temp_dir, "audio153.wav")
-# OUTPUT AUDIO name and path
-output_wav_name = "output_" + datetime.now().strftime("%H-%M-%S") + ".wav"
-output_wav_path = os.path.join(temp_dir, output_wav_name)
-# Output Sample rate
-sample_rate = 24000
-
-
-# XTTS Settings
-
-# Generate silence duration between sentences
-silence_duration = 0.3 #(seconds)
-silence_waveform = torch.zeros(int(sample_rate * silence_duration))
-# XTTS Parameters
-xtts_temp = 0.7
-xtts_spd = 0.95
-
-
-# RVC Settings
-
-#set name of rvc model here and it will look for it automatically
-# in the rvc models folder specified earlier
-rvc_model_name = "ranni2"
-
-rvc_model = RVCInference(models_dir = rvc_models_dir, device = "cuda:0")
-# RVC Parameters
-rvc_model.f0method = "rmvpe"
-rvc_model.rms_mix_rate = 0.25
-rvc_model.index_rate = 0.75
+    return sentences
 
 
 
 
 
-### INPUT TEXT HANDLING
+def main():
 
-# Load sentences.txt - temp folder by default
-with open(os.path.join(temp_dir, "sentences.txt"), "r", encoding = "utf-8") as file:
-    text = file.read()
+    ###* CONFIG
 
-# Split sentences apart for sequential batched inference
-"""
-HOW THIS WORKS:
-1. We use the `re` module for splitting text based on punctuation.
-2. The regex `(\.{3}|[.!?])` ensures we handle:
-    - Ellipses (`...`) as a single punctuation unit.
-    - or Full stops (`.`), exclamation marks (`!`), and question marks (`?`).
-3. Capturing groups `()` in the regex allow punctuation to be retained in the results rather than lost upon splitting.
-4. Whitespace and empty strings are removed by strip() to ensure clean results.
-5. How the if condition for .strip() works:
-    - Python accepts an empty sentence ('') as a result of .strip() as False, anything else true
-    - So it gets a true for non empty sentences and knows it can strip
-6. After splitting, we combine each sentence with its respective punctuation by iterating
-   through the list in pairs (sentence + punctuation) for the sake of the TTS engine.
-"""
-input_text = [sentence.strip() for sentence in re.split(r"(\.{3}|[.!?])", text) if sentence.strip()]
+    # Directory setup
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    #By default output audio goes to temp folder
+    temp_dir = os.path.join(base_dir, "temp")
 
-# combine sentences back with their punctuation mark
-sentences = [
-    input_text[i] + input_text[i + 1]
-    for i in range(0, len(input_text) - 1, 2)  # range(inclusive, exclusive, step)
-    #We stop it 1 element before the end to prevent error if last element has no punc.
-]
-print("SENTENCES:\n", sentences)
+
+    # Model file locations
+
+    #XTTS base model vocab file
+    xtts_vocab_path = os.path.join(base_dir, "voices\\xtts\\vocab.json")
+    #XTTS trained model checkpoint file
+    xtts_checkpoint_path = os.path.join(base_dir, "voices\\xtts\\Ranni1\\best_model.pth")
+    #XTTS trained model config
+    xtts_config_path = os.path.join(base_dir, "voices\\xtts\\Ranni1\\config.json")
+    #RVC trained model file location
+    rvc_models_dir = os.path.join(base_dir, "voices\\rvc")
 
 
 
 
 
-### XTTS
+    ###* PARAMETERS 
 
-# Init audio list that each generated XTTS sentence will be appended to
-joined_audio = []
+    # REFERENCE AUDIO - by default looks in temp folder
+    speaker_ref_path = os.path.join(temp_dir, "audio153.wav")
+    # OUTPUT AUDIO name and path
+    output_wav_name = "output_" + datetime.now().strftime("%H-%M-%S") + ".wav"
+    output_wav_path = os.path.join(temp_dir, output_wav_name)
+    # Output Sample rate
+    sample_rate = 24000
 
-# Init XTTS model
-print("\n Loading XTTS model...")
-config = XttsConfig()
-config.load_json(xtts_config_path)
-model = Xtts.init_from_config(config)
-model.load_checkpoint(config, checkpoint_path=xtts_checkpoint_path, vocab_path=xtts_vocab_path, use_deepspeed=False)
-model.cuda()
 
-print("Computing speaker latents...")
-gpt_cond_latent, speaker_embedding = model.get_conditioning_latents(audio_path=[speaker_ref_path])
+    # XTTS Settings
 
-# Inference
-print("Performing TTS generation")
-for sentence in sentences:
-    out = model.inference(
-        sentence,
-        "en",
-        gpt_cond_latent,
-        speaker_embedding,
-        temperature = xtts_temp,
-        speed = xtts_spd
-    )
+    # Generate silence duration between sentences
+    silence_duration = 0.3 #(seconds)
+    silence_waveform = torch.zeros(int(sample_rate * silence_duration))
+    # XTTS Parameters
+    xtts_temp = 0.7
+    xtts_spd = 0.95
 
-    #Hold output as variable so we can append it to joined audio
-    temp_waveform = torch.tensor(out["wav"])
 
-    joined_audio.append(temp_waveform)
-    joined_audio.append(silence_waveform)
+    # RVC Settings
 
-# Concatinate all audio waveforms together and save file
-joined_audio = torch.cat(joined_audio)
-torchaudio.save(output_wav_path, joined_audio.unsqueeze(0), sample_rate)
-print("XTTS inference complete!")
-print(f"Saved TTS output to: {output_wav_path}")
+    #set name of rvc model here and it will look for it automatically
+    # in the rvc models folder specified earlier
+    rvc_model_name = "ranni2"
+
+    rvc_model = RVCInference(models_dir = rvc_models_dir, device = "cuda:0")
+    # RVC Parameters
+    rvc_model.f0method = "rmvpe"
+    rvc_model.rms_mix_rate = 0.25
+    rvc_model.index_rate = 0.75
 
 
 
+    ###* INPUT TEXT HANDLING
+    sentences = textSplitter(temp_dir)
 
 
-### RVC
 
-#We initialised the rvc_model object earlier, now we load an actual model
-print("\nLoading RVC model...")
-rvc_model.load_model(rvc_model_name)
-print(f"Loaded RVC model: {rvc_model.models}")
+    ###* XTTS
 
-# Inference
-print("Performing RVC Inference...")
-rvc_model.infer_file(output_wav_path, output_wav_path)
-print(f"RVC Inference complete. Output: {output_wav_path}")
+    # Init audio list that each generated XTTS sentence will be appended to
+    joined_audio = []
+
+    # Init XTTS model
+    print("\n Loading XTTS model...")
+    config = XttsConfig()
+    config.load_json(xtts_config_path)
+    model = Xtts.init_from_config(config)
+    model.load_checkpoint(config, checkpoint_path=xtts_checkpoint_path, vocab_path=xtts_vocab_path, use_deepspeed=False)
+    model.cuda()
+
+    print("Computing speaker latents...")
+    gpt_cond_latent, speaker_embedding = model.get_conditioning_latents(audio_path=[speaker_ref_path])
+
+    # Inference
+    print("Performing TTS generation")
+    for sentence in sentences:
+        out = model.inference(
+            sentence,
+            "en",
+            gpt_cond_latent,
+            speaker_embedding,
+            temperature = xtts_temp,
+            speed = xtts_spd
+        )
+
+        #Hold output as variable so we can append it to joined audio
+        temp_waveform = torch.tensor(out["wav"])
+
+        joined_audio.append(temp_waveform)
+        joined_audio.append(silence_waveform)
+
+    # Concatinate all audio waveforms together and save file
+    joined_audio = torch.cat(joined_audio)
+    torchaudio.save(output_wav_path, joined_audio.unsqueeze(0), sample_rate)
+    print("XTTS inference complete!")
+    print(f"Saved TTS output to: {output_wav_path}")
+
+
+
+
+
+    ###* RVC
+
+    #We initialised the rvc_model object earlier, now we load an actual model
+    print("\nLoading RVC model...")
+    rvc_model.load_model(rvc_model_name)
+    print(f"Loaded RVC model: {rvc_model.models}")
+
+    # Inference
+    print("Performing RVC Inference...")
+    rvc_model.infer_file(output_wav_path, output_wav_path)
+    print(f"RVC Inference complete. Output: {output_wav_path}")
+
+
+
+if __name__ == "__main__":
+    main()
